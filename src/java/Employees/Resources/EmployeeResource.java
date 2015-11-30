db5380e74230d5a406b4ded69014cf5f5709a402
Boris Ablamunits <borisa@wix.com>
2015-11-30 22:16:49 +0200

    Testing out resources

---
+++ src/java/Employees/Resources/EmployeeResource.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Employees.Resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Path("/employees")
+public class EmployeeResource {
+@GET
+@Produces(MediaType.APPLICATION_JSON)
+    public String testFunction() throws JSONException {
+        JSONObject test = new JSONObject();
+        
+        test.put("id", "2").put("name", "Jerby");
+        
+        return test.toString();
+    }
+    
+    @GET
+    @Produces("text/plain")
+    @Path("/boris")
+    public String testFunction2() {
+        return "Hello boris";
+    }
+
+}
